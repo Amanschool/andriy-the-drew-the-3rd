c63959d47ec2aa2a8cfbfae911fe20c811bae37c
Amanschool <116610739+Amanschool@users.noreply.github.com>
2022-10-25 09:20:50 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Ghost)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
